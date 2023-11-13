4187d1c5544244c56b50be475c8c168e44a3175d
avilovmaksim5 <avilovmaksim5@mail.ru>
2023-11-13 20:30:42 +0400

    update 13/11/23

--- calc.js
+++ calc.js
@@ -15,8 +15,56 @@ buttons.forEach(function (button){
         result.classList.remove("active");
         let value = event.target.dataset.num;
         numbers.innerText += value;
-        result.innerText = "=" + eval(numbers.innerText.replace("x", "*")
-            .replace("÷", "/").replace("^", "**"));
-        
+        equals();
     })
+});
+
+equal.addEventListener('click', function (){
+    if (numbers.innerText === ''){
+        numbers.innerText = 0;
+    } else {
+        numbers.classList.remove("active");
+        result.classList.add("active");
+    }
+});
+
+allClear.addEventListener('click', remove);
+
+function remove(){
+    numbers.innerText = '';
+    result.innerText = 0;
+    numbers.classList.remove("active");
+    result.classList.add("active");
+};
+
+del.addEventListener('click', function (){
+    if (!result.classList.contains("active")){
+        if (numbers.innerText.length !== 1){
+            numbers.innerText = numbers.innerText.slice(0, -1);
+            result.innerText = result.innerText.slice(0, -1);
+            equals();
+        } else {
+            remove();
+        }
+    }
+});
+
+function equals(){
+    result.innerText = "=" + eval(numbers.innerText.replace("x", "*")
+        .replace("÷", "/").replace("^", "**"));
+};
+
+document.querySelector(".btn-modal").addEventListener('click', function (){
+    if (moreBtn.classList.contains("moreActive")) {
+        moreBtn.classList.remove("moreActive")
+    } else {
+        moreBtn.classList.add("moreActive");
+    }
+});
+
+btnTheme.addEventListener('click', function (){
+   if (theme.getAttribute('href') === './light.css') {
+       theme.href = './dark.css';
+       btnTheme.innerText = "🌞";
+   }
 });
